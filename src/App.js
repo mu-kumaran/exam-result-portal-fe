94077b2a08898c1e6a4e6c62901bdce08aa3e147
mu-kumaran <mu.kumaran1706@gmail.com>
2025-06-11 12:09:29 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+    const [rno,setRno] = useState(0)
+    const [dob,setDob] = useState("01/01/2001")
+
+    const dis=(e)=>{
+        if (e.target.name === "rno")
+            setRno(e.target.value)
+        if (e.target.name === "dob")
+            setDob(e.target.value)
+    }
+
+    const handleReset = () =>{
+        setRno(0)
+        setDob('01/01/2001')
+    }
   return (
     <>
         <title>XXXXX Board Results</title>
@@ -12,13 +26,13 @@ const App = () => {
             <h3>Examination Results - March 202X</h3>
         </div>
         <div>
-            <div>
+            <form>
                 <label for='rno'>Registration No: </label>
-                <input type='text' name='rno'></input><br></br><br></br>
+                <input type='text' name='rno' placeholder='Enter your registration no' value={rno} onChange={(e)=>dis(e)}></input><br></br><br></br>
                 <label for='dob'>Date of Birth (dd/mm/yyyy): </label>
-                <input type='text' name='dob'></input><br></br><br></br>
-                <button>Get Results</button> <button>Clear</button>
-            </div>
+                <input type='text' name='dob' placeholder='Enter your dob' value={dob} onChange={(e)=>dis(e)}></input><br></br><br></br>
+                <button>Get Results</button> <button onClick={handleReset}>Clear</button>
+            </form>
         </div>
         <div>
             <p>This is the test site designed by <a href='https://github.com/mu-kumaran'>Manoj Kumar Dharmalingam</a> using MERN Stack for project purpose and hosted using <a href='https://github.com/'>Github</a>, <a href='https://www.render.com/'>Render</a> and <a href='https://www.mongodb.com/products/platform/atlas-database'>MongoDB Atlas</a> platforms.</p>
